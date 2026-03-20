94f1fe6e72aa4018baec91b5745c0c93d6282d9a
Saksham Chanouria <chanouria.saksham003@gmail.com>
2026-03-20 16:08:44 +0530

    Default API Handling

--- HealthSynqBackend/src/main/java/com/health/HealthSynqBackend/controllers/DemoController.java
+++ HealthSynqBackend/src/main/java/com/health/HealthSynqBackend/controllers/DemoController.java
@@ -1,10 +1,16 @@
 package com.health.HealthSynqBackend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+    @GetMapping("/")
+    public String defaultResult(){
+        return "Server Bdhiya chl raha hai..... MAA CHUDAO";
+    }
+
     @GetMapping("/ping")
     public String ping(){
         return "OK";
